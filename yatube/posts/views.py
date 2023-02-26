b4f36bd65d83f631128ca3c8127837333e60dccb
Александр Рыбин <officialmma@mail.ru>
2023-02-26 20:28:45 +0300

    refactoring and bug fixing

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,11 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def headers(request):
-    template = 'includes/header.html'
-    return render(request, template)
-
-
 def index(request):
     template = 'posts/index.html'
     title = 'Это главная страница проекта Yatube'
@@ -18,19 +13,10 @@ def index(request):
     return render(request, template, context)
 
 
-def group(request):
+def group_posts(request, slug):
     template = 'posts/group_list.html'
     text = 'Здесь будет информация о группах проекта Yatube'
     context = {
-        'text': text
+        'text': text,
     }
     return render(request, template, context)
-
-
-def footer(request):
-    template = 'includes/footer.html'
-    return render(request, template)
-
-
-def group_posts(request, slug):
-    return HttpResponse(f'Тут что-то о Твиксе {slug}')

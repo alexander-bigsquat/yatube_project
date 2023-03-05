cf584ca65edab67682abfe6b512e97ddb85b3266
Александр Рыбин <officialmma@mail.ru>
2023-03-05 10:52:05 +0300

    Yatube

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -2,15 +2,23 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Post
+from .models import Group
+
+admin.site.register(Group)
 
 
 class PostAdmin(admin.ModelAdmin):
-    # Перечисляем поля, которые должны отображаться в админке
-    list_display = ('pk', 'text', 'pub_date', 'author')
-    # Добавляем интерфейс для поиска по тексту постов
+    list_display = (
+        'pk',
+        'text',
+        'pub_date',
+        'author',
+        'group',
+    )
+    list_editable = ('group',)
     search_fields = ('text',)
-    # Добавляем возможность фильтрации по дате
     list_filter = ('pub_date',)
-    empty_value_display = '-пусто-'  
+    empty_value_display = '-пусто-' 
+   
 
-admin.site.register(Post, PostAdmin)  
+admin.site.register(Post, PostAdmin)
--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -12,3 +12,19 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name='posts'
     )
+    group = models.ForeignKey(
+        'Group',
+        blank=True, null=True,
+        on_delete=models.CASCADE,
+        )
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return Group.title
+    
+    
